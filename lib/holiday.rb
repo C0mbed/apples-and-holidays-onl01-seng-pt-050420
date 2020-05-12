f9ee589c39e0a409cf6097862f0f931f737fabb4
Matt Farncombe <matt@farncombe.co>
2020-05-12 18:29:54 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -83,7 +83,6 @@ def all_supplies_in_holidays(holiday_hash)
       end
         
       supplies.each do |supply|
-        supply_format = []
         if supply =~ /[\s]/
           supply_array = supply.split(" ")
           supply_array.each do |word|
