12aa7735e92952de496bc43082431959315cc70e
Matt Farncombe <matt@farncombe.co>
2020-05-12 13:05:55 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,6 +65,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
 
   holiday_hash.each do |season, data|
+    holiday_result = ""
     puts "#{season.to_s.capitalize}:"
     data.each do |holiday, supplies|
       if holiday =~ /["-"]/
