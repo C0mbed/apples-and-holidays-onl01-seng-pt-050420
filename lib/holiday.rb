d13559a575cd77de0b8cf1ce8f4be39775596fce
Matt Farncombe <matt@farncombe.co>
2020-05-12 12:21:32 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,7 +66,7 @@ def all_supplies_in_holidays(holiday_hash)
 
   holiday_hash.each do |season, data|
    season_as_a_string = season.to_s
-   puts "#{season.to_s.capitalize}:"
+   puts "#{season.titleize}:"
    data.each do |holiday, supplies|
      capitalized_supply = []
      supplies.each do |supply|
