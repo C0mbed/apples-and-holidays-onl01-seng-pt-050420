fc09c476f924bcc48d57edaf1da94f0116a81652
Matt Farncombe <matt@farncombe.co>
2020-05-12 16:56:08 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -95,7 +95,7 @@ def all_supplies_in_holidays(holiday_hash)
           supply_format << supply.to_s.capitalize
         end
       end
-      supply_format = supply_format.join(" ")
+      supply_result = supply_format.join(" ")
       puts "  #{holiday_result}: #{supply_result}"
     end
   end
